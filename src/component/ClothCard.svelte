b8c1fa151d255e4f5de7281cc58727e94e9859ac
galiev <ggvalion@gmail.com>
2024-06-18 19:37:57 +0300

    Added image renderer #4

--- src/component/ClothCard.svelte
+++ src/component/ClothCard.svelte
@@ -1,26 +1,58 @@
 <script lang="ts">
-    import { Badge, Button, Card, Group, Image, Text } from '@svelteuidev/core';
-    export let name: string, link: string, description: string;
+    import {Alert, Button, Card, Group, Image, Loader, Text} from '@svelteuidev/core';
+    import { InfoCircled } from 'radix-icons-svelte';
+    import { onMount } from "svelte";
+    import type ImageDTO from "../dto/Image";
+
+    export let image_id: number, name: string, link: string, description: string;
+
+    let image: ImageDTO | null = null;
+    let error: string | null = null;
+
+    async function fetchImage(id: number): Promise<Image | null> {
+        try {
+            const response = await fetch(`http://0.0.0.0:8080/api/v1/images/${id}`);
+            if (!response.ok) {
+                throw new Error('Failed to fetch image: ' + response.statusText);
+            }
+            return await response.json();
+        } catch (err: any) {
+            error = err.message;
+            return null;
+        }
+    }
+
+    onMount(async () => {
+        image = await fetchImage(image_id);
+    });
 </script>
+{#if error}
+    <Alert icon={InfoCircled}  title="Oopsie!" color="red">
+        {error}
+    </Alert>
+{:else if image}
+    <Card shadow='sm' padding='lg'>
+            <Card.Section first padding='lg'>
+                <Image
+                        src={`data:image/png;base64,${image.bytes}`}
+                        height={160}
+                        alt={image.name}
+                        id="img"
+                />
+            </Card.Section>
 
-<Card shadow='sm' padding='lg'>
-    <Card.Section first padding='lg'>
-        <Image
-                src='./image.png'
-                height={160}
-                alt='Portugal'
-        />
-    </Card.Section>
+        <Group position='apart'>
+            <Text weight={500}>{name}</Text>
+        </Group>
 
-    <Group position='apart'>
-        <Text weight={500}>{name}</Text>
-    </Group>
+        <Text size='sm'>
+            {description}
+        </Text>
 
-    <Text size='sm'>
-        {description}
-    </Text>
-
-    <Button variant='light' color='blue' href="{link}" fullSize>
-        Buy now
-    </Button>
-</Card>
+        <Button variant='light' color='blue' href="{link}" fullSize>
+            Show
+        </Button>
+    </Card>
+{:else}
+    <Loader />
+{/if}
--- src/routes/Clothes.svelte
+++ src/routes/Clothes.svelte
@@ -1,17 +1,31 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import ClothCard from "../component/ClothCard.svelte";
+    import type Clothes from "../dto/Clothes";
 
-    let clothes: any = []
+    let clothes: Clothes[] | [] = []
+    let error: string | null = null
+
+    async function fetchClothes(): Promise<Clothes[] | []> {
+        try {
+            const response = await fetch(`http://0.0.0.0:8080/api/v1/clothes`);
+            if (!response.ok) {
+                throw new Error('Failed to fetch image: ' + response.statusText);
+            }
+            return await response.json();
+        } catch (err: any) {
+            error = err.message;
+            return [];
+        }
+    }
 
     onMount(async () => {
-        const response = await fetch('https://e2484b7d-baa7-4b5f-a860-cf269e3bb116.mock.pstmn.io/api/v1/clothes')
-        clothes = await response.json()
+        clothes = await fetchClothes()
     })
 
 </script>
 
 <h1>Clothes</h1>
 {#each clothes as cloth (cloth.id)}
-    <ClothCard name="{cloth.name}" link="{cloth.link}" description="{cloth.description}"></ClothCard>
+    <ClothCard image_id="{cloth.image_id}" name="{cloth.name}" link="{cloth.link}" description="{cloth.description}"></ClothCard>
 {/each}
