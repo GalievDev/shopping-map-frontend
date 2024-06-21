7caeca045edcbc2e4799c4e227c4206d2f18236a
Rostislav Kirsanov <r.kirsanov@innopolis.university>
2024-06-21 15:32:19 +0300

    Главная страница. Изменил расположение кнопок и упростил код связаный с переходом по ссылкам

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -5,39 +5,41 @@
     import {  AspectRatio, Image } from '@svelteuidev/core';
     export let vertical_item_and_capslue = '130px'
     export let horizontal_item_and_capslue = '230px'
-    export let vertical_outfit = '70px'
-    export let horizontal_outfit = '85px'
-    function navigate_clothes(){
-        window.location.href = 'http://localhost:5173/#/clothes';
-    }
-    function navigate_capsule(){
-        window.location.href = 'http://localhost:5173/#/capsules';
-    }
+    export let vertical_outfit = '100px'
+    export let horizontal_outfit = '110px'
 </script>
 <h1>Home page</h1>
-<!-- MyLayout.svelte -->
 <style>
     .grid {
         display: grid;
         grid-template-columns: 1fr 1fr 1fr; /* Сетка из 3 колонок */
-        gap: 100px; /* Пространство между элементами сетки */
-        margin-top: 60px; /* Отступ сверху для сетки, чтобы отделить её от стека */
+        gap: 50px; /* Пространство между элементами сетки */
+        margin-top: 35px; /* Отступ сверху для сетки, чтобы отделить её от стека */
+        margin-left: 60px;
     }
 </style>
-
-
+<Grid>
+    <Grid.Col span={5} offset={0.5}>
+        <Input color="red"
+               icon={MagnifyingGlass}
+               placeholder='Поиск...'
+               rightSectionWidth={70}
+               styles={{ rightSection: { pointerEvents: 'none' } }}>
+        </Input>
+    </Grid.Col>
+</Grid>
 <div class="grid">
-        <Button style="padding: {vertical_item_and_capslue} {horizontal_item_and_capslue};" color="gray" on:click={navigate_clothes}>
+        <Button style="padding: {vertical_item_and_capslue} {horizontal_item_and_capslue};" color="gray" href="http://localhost:5173/#/clothes">
         Добавить предмет
     </Button>
-        <Button style="padding: {vertical_outfit} {horizontal_outfit};">
+        <Button style="padding: {vertical_outfit} {horizontal_outfit};" >
         1
     </Button>
         <Button style="padding: {vertical_outfit} {horizontal_outfit};">
             2
         </Button>
 
-        <Button style="padding: {vertical_item_and_capslue} {horizontal_item_and_capslue};" color="gray" on:click={navigate_capsule}>
+        <Button style="padding: {vertical_item_and_capslue} {horizontal_item_and_capslue};" color="gray" href="http://localhost:5173/#/capsules">
         Создать новую капсулу
     </Button>
         <Button style="padding: {vertical_outfit} {horizontal_outfit};">
@@ -47,26 +49,3 @@
         <Button style="padding: {vertical_outfit} {horizontal_outfit};">
             4</Button>
 </div>
-
-<Grid>
-    <Grid.Col span={5} offset={0.7}>
-        <Input color="gray"
-                icon={MagnifyingGlass}
-                placeholder='Поиск...'
-                rightSectionWidth={70}
-                styles={{ rightSection: { pointerEvents: 'none' } }}>
-        </Input>
-    </Grid.Col>
-</Grid>
-<SimpleGrid cols = {3}>
-
-
-
-
-
-
-
-
-
-
-</SimpleGrid>
