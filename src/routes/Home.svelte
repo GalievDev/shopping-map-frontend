ec0f6d39064d1e0385fd115b73d9d46537424fa7
Rostislav Kirsanov <r.kirsanov@innopolis.university>
2024-06-21 11:31:54 +0300

    Добавлен поиск. Добавлена кнопка добавить предмет, создать новую капсулу, добавлены 4 кнопки аутфита. У кнопки добавить предмет и создать новую капсулу выполнается действие по переходу в вкладки Clothes и Capsule

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,1 +1,56 @@
+<script lang="ts">
+    import { EnvelopeClosed } from 'radix-icons-svelte';
+    import { Button, Flex, Grid, Input, SimpleGrid } from '@svelteuidev/core';
+    import { MagnifyingGlass } from 'radix-icons-svelte';
+    import {  AspectRatio, Image } from '@svelteuidev/core';
+    export let vertical_item_and_capslue = '130px'
+    export let horizontal_item_and_capslue = '230px'
+    export let vertical_outfit = '70px'
+    export let horizontal_outfit = '85px'
+    function navigate_clothes(){
+        window.location.href = 'http://localhost:5173/#/clothes';
+    }
+    function navigate_capsule(){
+        window.location.href = 'http://localhost:5173/#/capsules';
+    }
+</script>
 <h1>Home page</h1>
+<Grid>
+    <Grid.Col span={5} offset={0.7}>
+        <Input color="gray"
+                icon={MagnifyingGlass}
+                placeholder='Поиск...'
+                rightSectionWidth={70}
+                styles={{ rightSection: { pointerEvents: 'none' } }}>
+        </Input>
+    </Grid.Col>
+</Grid>
+<SimpleGrid cols = {3}>
+    <Button style="padding: {vertical_item_and_capslue} {horizontal_item_and_capslue};" color="gray" on:click={navigate_clothes}>
+        Добавить предмет
+    </Button>
+
+        <Button style="padding: {vertical_outfit} {horizontal_outfit};">
+            1
+        </Button>
+
+        <Button style="padding: {vertical_outfit} {horizontal_outfit};">
+            2
+        </Button>
+
+    <Button style="padding: {vertical_item_and_capslue} {horizontal_item_and_capslue};" color="gray" on:click={navigate_capsule}>
+        Создать новую капсулу
+    </Button>
+
+        <Button style="padding: {vertical_outfit} {horizontal_outfit};">
+            3
+        </Button>
+
+        <Button style="padding: {vertical_outfit} {horizontal_outfit};">
+            4
+        </Button>
+
+
+
+
+</SimpleGrid>
